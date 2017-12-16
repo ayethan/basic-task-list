57c847d38794a8db8a37011aa475aed9055a7e05
ayethan <hainthantkyaw.com>
2017-12-16 17:49:01 +0630

    finish basic task

--- basictask/routes/web.php
+++ basictask/routes/web.php
@@ -1,16 +1,32 @@
 <?php
+use App\Task;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/',function(){
 
-Route::get('/', function () {
-    return view('welcome');
+	$Bello = Task::orderBy('created_at','asc')->get();
+	return view('tasks',['tasks'=>$Bello
+		]);
+});
+
+Route::post('/task',function(Request $request){
+	$validator = \Validator::make($request->all(),[
+	     'name' => 'required|min:3',	]);
+	if ($validator->fails()) {
+		return redirect('/')
+		       ->withInput()
+		       ->withErrors($validator);
+		
+	}
+
+	$task = new Task;
+	$task->name = $request->name;
+	$task->save();
+
+	return redirect('/');
+});
+
+Route::delete('/task/{task}', function(Task $task){
+  $task->delete();
+  return redirect('/');
 });
